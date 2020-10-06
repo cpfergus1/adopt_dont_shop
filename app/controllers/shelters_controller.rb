2bc71b98e64fa9d0554fc6911b6df970daa6f812
Connor Ferguson <cpfergus1@gmail.com>
2020-10-06 20:22:55 -0300

    Implemented shelter class and updated shelter webpage to display shelter object links

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,8 +1,24 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ["shelter1", "shelter2"] #Shelters.all
+    @shelters = Shelter.all
+  end
+
+  def show
+    @shelters.find(params[:id])
   end
 
   def new
+  end
+
+  def create
+    task = Task.new({
+    name: params[:shelters][:name],
+    address: params[:shelters][:address],
+    city: params[:shelters][:city],
+    state: params[:shelters][:state],
+    zip: params[:shelters][:zip]
+    })
+    shelter.save
+    redirect_to '/shelters'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
+  get '/shelters/:id', to: "shleters#show"
 end
