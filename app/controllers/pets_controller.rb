9f6f3a3b9e3dfedff5802882b3917ac0b1cdfccc
Connor Ferguson <cpfergus1@gmail.com>
2020-10-07 21:54:12 -0300

    implemented show page for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,13 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def destroy
+    Pet.destroy(params[:id])
+    redirect_to '/pets'
+  end
 end
