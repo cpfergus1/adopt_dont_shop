67033b5094bd444bf15b60a43c85cbd42c383f04
Connor Ferguson <cpfergus1@gmail.com>
2020-10-07 21:25:55 -0300

    implemented sytle sheet and pets page with pets

---
+++ db/migrate/20201007215357_create_pets.rb
@@ -0,0 +1,15 @@
+class CreatePets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pets do |t|
+      t.string :image
+      t.string :name
+      t.string :description
+      t.integer :age
+      t.string :sex
+      t.string :status
+      t.integer :shelter_id
+
+      t.timestamp
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_002554) do
+ActiveRecord::Schema.define(version: 2020_10_07_215357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "image"
+    t.string "name"
+    t.string "description"
+    t.integer "age"
+    t.string "sex"
+    t.string "status"
+    t.integer "shelter_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
