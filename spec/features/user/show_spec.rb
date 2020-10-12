b8a5a5874137374b49bb7e35c3db6da2454c2a77
Connor Ferguson <cpfergus1@gmail.com>
2020-10-12 19:28:34 -0300

    Implement user clas

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 #User Story 2, User Show Page
 
-describe 'As a visitor'
-  describe "When I visit a User's show page"
-    it "Then I see all that User's information"
+describe 'As a visitor' do
+  before(:each) do
+    @user = User.create(name: "name",
+                        address: "address",
+                        city: "city",
+                        state: "state",
+                        zip: 8)
+  end
+  describe "When I visit a User's show page" do
+    it "Then I see all that User's information" do
       #Including the User's
-expect(page).to have_content(@user.name)
-expect(page).to have_content(@user.street)
-expect(page).to have_content(@user.city)
-expect(page).to have_content(@user.state)
-expect(page).to have_content(@user.zip)
+      visit "/users/#{@user.id}"
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@user.street)
+      expect(page).to have_content(@user.city)
+      expect(page).to have_content(@user.state)
+      expect(page).to have_content(@user.zip)
+    end
+  end
+end
