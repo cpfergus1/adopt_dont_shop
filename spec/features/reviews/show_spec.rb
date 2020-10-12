8b0d5e724919c36dbf6c9099033f85c0c77df146
Connor Ferguson <cpfergus1@gmail.com>
2020-10-12 20:46:33 -0300

    Implemented test for review

---
+++ spec/features/reviews/show_spec.rb
@@ -0,0 +1,28 @@
+#User Story 4, Shelter Reviews
+require 'rails_helper'
+
+describe "As a visitor," do
+  before(:each) do
+    @shelter = Shelter.create(name: 'Windsor',
+                            address: 'address',
+                            city: 'city',
+                            state: 'state',
+                            zip: '8')
+    @review = Review.create(title: "this review",
+                            rating: 3.5,
+                            content: "This review needs work",
+                            user_name: "Joseph"
+                            )
+  end
+  describe "When I visit a shelter's show page," do
+    it "I see a list of reviews for that shelter" do
+    #Each review will have:
+      visit "/shelters/#{@shelter.id}"
+      expect(page).to have_content(@review.title)
+      expect(page).to have_content(@review.rating)
+      expect(page).to have_content(@review.content)
+      expect(page).to have_content(@review.pic)
+      expect(page).to have_content(@review.user_name)
+    end
+  end
+end
