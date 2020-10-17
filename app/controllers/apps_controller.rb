4eeee9e7d3c8f0b3d3a291143545c69e449c8ec7
Connor Ferguson <cpfergus1@gmail.com>
2020-10-17 18:54:31 -0300

    implemented new application functionality

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,4 +5,20 @@ class AppsController < ApplicationController
     @user = @application.user
     @pets = @application.pets
   end
+
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+    app = App.new(status: 'In Progress',
+                  user_id: user.id)
+    if app.save
+      redirect_to "/apps/#{app.id}"
+    else
+      flash[:notice] = "Application not Created: User not found or no entry"
+      render :new
+    end
+  end
+
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 describe App, type: :model do
   describe "validations" do
     it do
-      should validate_presence_of :description
       should validate_presence_of :status
       should have_many :pet_apps
       should have_many(:pets).through(:pet_apps)
